8d5628acc2704cbc69140339d15217394f57f96a
ChanonChanom <chanonchanom@gmail.com>
2015-05-26 18:18:01 +0700

    category2

---
+++ resources/views/layout/category.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('bootflat/css/bootflat.css') !!}">
+
+    <link rel="stylesheet" href="{!! asset('css/demo.css') !!}">
+    </head>
+
+<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+          
+  
+
+  <body>
+    <!-- Navbars   ================================================== -->
+    <div class="row">
+              <div class="col-md-12">
+                <nav class="navbar navbar-inverse" role="navigation">
+                  <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-5">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="#">Home</a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-5">
+                      <ul class="nav navbar-nav">
+                        <li>
+                        <a class="navbar-brand" href="#">Browse Course</a></li>
+                        <!-- <li class="disabled"><a href="#">Link</a></li> -->
+                        <li>
+                          <a class="navbar-brand" href="#">Blog</a>
+                        </li>
+                        <li>
+                          <a class="navbar-brand" href="#">Payment</a>
+                        </li>
+                          </ul>
+                        </li>
+                      </ul>
+                      <form class="navbar-form navbar-right" role="search">
+                        <div class="form-search search-only">
+                          <i class="search-icon glyphicon glyphicon-search"></i>
+                          <input type="text" class="form-control search-query">
+                        </div>
+                      </form>
+                    </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+                </nav>
+              </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<ul>
+
+</ul>
+
+
+<!--     Footer   -->  
+    <div class="footer" style="background-color:#101010 ;height:40px;">
+        <div class="col-lg-12">
+
+            <ul class="nav nav-pills nav-justified">
+                <a class="grey-text text-lighten-5 " style="color:#3333FF ;height:40px;" href="#">About us</a>
+                
+
+                <li align = "right"> © 2015 Company Name.</li>
+
+
+            </ul>
+      
+        </div>
+    </div>
+
+
+
+
+</body>
+
+    
+
+</html>
+
+
+
+<!-- Bootstrap -->
+    <script src="{!! asset('js/jquery.min.js') !!}"></script>
+    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+
+    <!-- Bootflat's JS files.-->
+    <script src="{!! asset('bootflat/js/icheck.min.js') !!}"></script>
+    <script src="{!! asset('bootflat/js/jquery.fs.selecter.min.js') !!}"></script>
+    <script src="{!! asset('bootflat/js/jquery.fs.stepper.min.js') !!}"></script>
+
+
